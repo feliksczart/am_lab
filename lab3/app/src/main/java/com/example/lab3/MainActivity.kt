4647335a723487aa2b2e27f75ae1a303f542bd6f
Feliks Czart <czart78@gmail.com>
2021-03-15 18:28:42 +0100

    no warnings

--- lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lab3
 
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
+import android.R.color
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.Color.red
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +25,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val renewButton = findViewById<ImageButton>(R.id.renewButton)
-        renewButton.drawable.mutate().setColorFilter(0xFF9871DE.toInt(), PorterDuff.Mode.MULTIPLY)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            renewButton.drawable.colorFilter =
+                BlendModeColorFilter(Color.parseColor("#9871de"), BlendMode.SRC_ATOP)
+        }
 
         draw()
 
@@ -30,11 +39,17 @@ class MainActivity : AppCompatActivity() {
         val zeroButton = findViewById<Button>(R.id.zeroButton)
 
         guessButton.setOnClickListener {
-            val number = input.text.toString().toInt()
+            val inputText = input.text.toString()
+
+            val number = if (inputText == "") {
+                100
+            } else {
+                inputText.toInt()
+            }
 
             val count = check(input, number, yourScore, drawnCount)
             if (count == 0) drawnCount.text = ""
-            else drawnCount.text = count.toString()
+            else if (count in 1..20) drawnCount.text = count.toString()
 
             input.text.clear()
         }
@@ -43,18 +58,18 @@ class MainActivity : AppCompatActivity() {
             reset(input, yourScore, drawnCount)
         }
 
-        zeroButton.setOnClickListener{
+        zeroButton.setOnClickListener {
             score = 0
             yourScore.text = "0"
         }
     }
 
-    fun draw() {
+    private fun draw() {
         shotCount = 0
         drawnNumber = Random.nextInt(0, 20)
     }
 
-    fun scoring(input: EditText, yourScore: TextView, drawnCount: TextView) {
+    private fun scoring(input: EditText, yourScore: TextView, drawnCount: TextView) {
         var pts = 0
         when (shotCount) {
             1 -> pts = 5
@@ -68,32 +83,61 @@ class MainActivity : AppCompatActivity() {
             9 -> pts = 1
             10 -> pts = 1
             else -> {
-//                showLost();
-                reset(input, yourScore,drawnCount)
+//                showLost()
+                reset(input, yourScore, drawnCount)
             }
         }
         points = pts
     }
 
-    private fun check(input: EditText, number: Int, yourScore: TextView, drawnCount: TextView): Int {
-        if (number == drawnNumber) {
-            scoring(input, yourScore, drawnCount)
-            Toast.makeText(applicationContext, "Trafiłeś! Zdobywasz " + points + " punktów", Toast.LENGTH_LONG).show()
-            score += points
-            yourScore.text = "$score"
-            draw()
-        } else {
-            if (number < drawnNumber) {
-                Toast.makeText(applicationContext, "Daj więcej", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(applicationContext, "Daj mniej", Toast.LENGTH_LONG).show()
-            }
-            shotCount++
-            if (shotCount > 10){
+    private fun check(
+        input: EditText,
+        number: Int,
+        yourScore: TextView,
+        drawnCount: TextView
+    ): Int? {
+        when (number) {
+            drawnNumber -> {
                 scoring(input, yourScore, drawnCount)
+                Toast.makeText(
+                    applicationContext,
+                    "Trafiłeś! Zdobywasz $points pkt!",
+                    Toast.LENGTH_LONG
+                ).show()
+                score += points
+                yourScore.text = "$score"
+                draw()
+                return shotCount
+            }
+            in 0..20 -> {
+                if (number < drawnNumber) {
+                    val toast = Toast.makeText(applicationContext, "Daj więcej", Toast.LENGTH_LONG)
+                    toast.setGravity(Gravity.TOP, 0, 0)
+                    toast.show()
+
+                } else {
+                    val toast = Toast.makeText(applicationContext, "Daj mniej", Toast.LENGTH_LONG)
+                    toast.setGravity(Gravity.TOP, 0, 0)
+                    toast.show()
+                }
+                shotCount++
+                if (shotCount > 10) {
+                    scoring(input, yourScore, drawnCount)
+                }
+                return shotCount
+            }
+            else -> {
+                println()
+                val toast = Toast.makeText(
+                    applicationContext,
+                    "Liczba musi być z przedziału od 0 do 20!",
+                    Toast.LENGTH_LONG
+                )
+                toast.setGravity(Gravity.TOP, 0, 0)
+                toast.show()
+                return null
             }
         }
-        return shotCount
     }
 
     private fun reset(input: EditText, yourScore: TextView, drawnCount: TextView) {
