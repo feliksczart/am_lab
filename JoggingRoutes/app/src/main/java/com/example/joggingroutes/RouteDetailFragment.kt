47d2325e62acecdc3f417011c12a335194f13803
Feliks Czart <czart78@gmail.com>
2021-05-02 00:28:50 +0200

    tablet done

--- JoggingRoutes/app/src/main/java/com/example/joggingroutes/RouteDetailFragment.kt
+++ JoggingRoutes/app/src/main/java/com/example/joggingroutes/RouteDetailFragment.kt
@@ -1,16 +1,26 @@
 package com.example.joggingroutes
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
 import kotlin.properties.Delegates
 
 
-class RouteDetailFragment : Fragment() {
+@Suppress("DEPRECATION")
+class RouteDetailFragment : Fragment(), OnMapReadyCallback {
     private var routeId by Delegates.notNull<Int>()
+    lateinit var coords: MutableList<LatLng>
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
@@ -39,11 +49,27 @@ class RouteDetailFragment : Fragment() {
             title.text = route.getName()
             val description = view.findViewById<View>(R.id.textDescription) as TextView
             description.text = route.getLength()
+
+            val mapFragment = childFragmentManager.findFragmentById(R.id.map_frag_tab) as? SupportMapFragment
+            mapFragment?.getMapAsync(this)
+
+            coords = route.getRouteCoords()
         }
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         savedInstanceState.putLong("routeId", routeId.toLong())
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        for (i in 0 .. coords.size-2){
+            val line = PolylineOptions().add(coords[i],coords[i+1])
+                .width(7f).color(Color.RED)
+            map.addPolyline(line)
+        }
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(coords[0], 16f))
     }
 
 }
