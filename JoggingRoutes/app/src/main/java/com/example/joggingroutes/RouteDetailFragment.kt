cda67e6d77bac6498ada2830d55a1b9776899ee6
Feliks Czart <czart78@gmail.com>
2021-05-11 21:30:58 +0200

    stoper fragment

--- JoggingRoutes/app/src/main/java/com/example/joggingroutes/RouteDetailFragment.kt
+++ JoggingRoutes/app/src/main/java/com/example/joggingroutes/RouteDetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -26,6 +27,13 @@ class RouteDetailFragment : Fragment(), OnMapReadyCallback {
         super.onCreate(savedInstanceState);
         if (savedInstanceState != null) {
             routeId = savedInstanceState.getLong("routeId").toInt()
+        } else {
+            val stoper = StoperFragment()
+            val ft: FragmentTransaction = childFragmentManager.beginTransaction()
+            ft.add(R.id.stoper_container, stoper)
+            ft.addToBackStack(null)
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            ft.commit()
         }
     }
 
@@ -67,7 +75,7 @@ class RouteDetailFragment : Fragment(), OnMapReadyCallback {
         map = googleMap
 
         for (i in 0 .. coords.size-2){
-            val line = PolylineOptions().add(coords[i],coords[i+1])
+            val line = PolylineOptions().add(coords[i], coords[i + 1])
                 .width(7f).color(Color.RED)
             map.addPolyline(line)
         }
---
+++ JoggingRoutes/app/src/main/java/com/example/joggingroutes/StoperFragment.kt
@@ -0,0 +1,103 @@
+package com.example.joggingroutes
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+
+@Suppress("DEPRECATION")
+class StoperFragment : Fragment(), View.OnClickListener {
+    var seconds: Int = 0
+    var running: Boolean = false
+    var wasRunning: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds")
+            running = savedInstanceState.getBoolean("running")
+            wasRunning = savedInstanceState.getBoolean("wasRunning")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        SavedInstanceState: Bundle?
+    ): View {
+        val layout: View = inflater.inflate(R.layout.fragment_stoper, container, false)
+        runStoper(layout)
+        val startButton: Button = layout.findViewById<View>(R.id.start_button) as Button
+        startButton.setOnClickListener(this)
+        val stopButton: Button = layout.findViewById<View>(R.id.stop_button) as Button
+        stopButton.setOnClickListener(this)
+        val resetButton: Button = layout.findViewById<View>(R.id.reset_button) as Button
+        resetButton.setOnClickListener(this)
+        return layout
+    }
+
+    override fun onPause() {
+        super.onPause()
+        wasRunning = running
+        running = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (wasRunning) {
+            running = true
+        }
+
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putInt("seconds", seconds)
+        savedInstanceState.putBoolean("running", running)
+        savedInstanceState.putBoolean("wasRunning", wasRunning)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.start_button -> onClickStart()
+            R.id.stop_button -> onClickStop()
+            R.id.reset_button -> onClickReset()
+        }
+    }
+
+    private fun onClickStart() {
+        running = true
+    }
+
+    private fun onClickStop() {
+        running = false
+    }
+
+    private fun onClickReset() {
+        running = false
+        seconds = 0
+    }
+
+    private fun runStoper(view: View) {
+        val timeView = view.findViewById<View>(R.id.time_view) as TextView
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = seconds % 3600 / 60
+                val secs = seconds % 60
+                val time = String.format("%d:%02d:%02d", hours, minutes, secs)
+                timeView.text = time
+                if (running) {
+                    seconds++
+                }
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+}
