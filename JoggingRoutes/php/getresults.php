c37cd70ff56a8c2ff843c78b3c489679a08315b9
Feliks Czart <czart78@gmail.com>
2021-05-14 23:41:27 +0200

    done

--- JoggingRoutes/php/getresults.php
+++ JoggingRoutes/php/getresults.php
@@ -14,7 +14,7 @@ if(mysqli_num_rows($res)>0){
     $u_id = $row[0];
 }
 
-$sql_query2="select result, date from results where id_user like '$u_id' and route like '$route' order by result limit 1;";
+$sql_query2="select result, date from results where id_user like '$u_id' and route like '$route' order by cast(result as int) limit 1;";
 $max_result = mysqli_query($connection,$sql_query2);
 $row2=mysqli_fetch_array($max_result);
 
@@ -37,9 +37,17 @@ if(mysqli_num_rows($max_result)>0){
 }
 
 if(mysqli_num_rows($last_result)>0){
-    $response = $response . $recent_result . ',' . $recent_result_date;
+    $response = $response . $recent_result . ',' . $recent_result_date . ';';
 } else {
-    $response = $response . "None";
+    $response = $response . "None" . ';';
+}
+
+$sql_query4="select login, result, date from results join users using(id_user) where route like '$route' order by cast(result as int);";
+$all_results = mysqli_query($connection,$sql_query4);
+if(mysqli_num_rows($all_results) > 0 ){
+    while ($row = mysqli_fetch_array($all_results)) {
+        $response = $response . $row[0] . ',' . $row[1] . ',' . $row[2] . '.';
+    }
 }
 
 echo $response;
